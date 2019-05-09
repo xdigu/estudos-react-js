450016fb21ff719251253217e0b2f4473bf3e287
Rodrigo jorge <xdigu@live.com>
2019-05-09 14:39:37 -0300

    Adicionado paginas de produto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './styles.css'
 
 import Header from './components/Header';
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -7,20 +7,45 @@ import api from './../../services/api'
 export default class Main extends Component {
     state = {
         products: [],
+        page: 1,
+        productInfo: {}
     };
 
+    // 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
-        this.setState({ products: response.data.docs })
-        console.log(response);
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`),
+            { docs: products, ...productInfo } = response.data;
+
+        this.setState({ products, productInfo, page });
     };
 
+    nextPage = () => {
+        const { page, productInfo } = this.state,
+            nextPage = page + 1;
+
+        if (page === productInfo.pages) return;
+
+        this.loadProducts(nextPage);
+    };
+
+    prevPage = () => {
+        const { page } = this.state,
+            nextPage = page - 1;
+
+        if (page === 1) return;
+
+        this.loadProducts(nextPage);
+    };
+
+
     render() {
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state,
+            productsPage = productInfo.pages,
+            totalProducts = productInfo.total;
 
         return (
             <div>
@@ -34,6 +59,13 @@ export default class Main extends Component {
                                 <a href=" ">Acessar</a>
                             </article>
                         ))}
+                    </div>
+                    <div className="total-product">
+                        <p>Total de produtos: {totalProducts}</p>
+                    </div>
+                    <div className="actions">
+                        <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                        <button disabled={page === productsPage} onClick={this.nextPage}>Próximo</button>
                     </div>
                 </div>
             </div>
